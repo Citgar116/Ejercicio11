95e5b9612dd2fef691b741f39b8ffbcbdd6b287e
Citgar116 <citlaligarcia723@gmail.com>
2026-05-09 23:58:02 -0600

    Arreglo de aplicacion

--- src/main/java/com/upiiz/Ejercicio11/Controllers/CalculadoraController.java
+++ src/main/java/com/upiiz/Ejercicio11/Controllers/CalculadoraController.java
@@ -3,14 +3,11 @@ package com.upiiz.Ejercicio11.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
-
 public class CalculadoraController {
-
-    @GetMapping()
+    @GetMapping("/calculadora")
     public String mostrarCalculadora() {
         return "calculadora";
-    }  
-    
+    }
+
 }
